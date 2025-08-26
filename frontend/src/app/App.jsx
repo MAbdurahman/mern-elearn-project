a996fa14cfbbf3479b2f3b230cbe0f0dfacb9db9
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-08-26 08:03:35 -0500

    Add frontend/src/context/AppContext.jsx and temporary adjust styles in frontend/src/index.css

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,8 +1,8 @@
 export default function App() {
 
    return (
-      <div>
-         <h2 className="text-3xl font-bold underline">App</h2>
+      <div className="app">
+         <h2 className="text-3xl text-center font-bold underline">App</h2>
       </div>
 
    );
---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+   const backendURL = import.meta.env.VITE_BACKEND_URL;
+   const currency = import.meta.env.VITE_CURRENCY;
+
+
+
+
+
+   return (
+      <AppContext.Provider value={value}>
+         {props.children}
+      </AppContext.Provider>
+   )
+}
