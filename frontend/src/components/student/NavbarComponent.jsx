b2b6c7e88fe2b18dd3c6c3abef1d97aed0d02661
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-08-27 11:52:12 -0500

    Add clerk package and complete functionality for frontend/src/components/student/NavbarComponent.jsx

--- frontend/src/components/student/NavbarComponent.jsx
+++ frontend/src/components/student/NavbarComponent.jsx
@@ -1,34 +1,59 @@
-import React, {useContext} from 'react';
+import React, {Fragment, useContext} from 'react';
 import {assets} from '../../assets/assets.js';
 import { Link, useLocation } from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 import { toast } from 'react-toastify';
 import axios from 'axios';
 
 export default function NavbarComponent() {
    const location = useLocation();
- let isEducator = true;
    const isCoursesListPage = location.pathname.includes('/course-list');
+let isEducator = false;
+   const { openSignIn } = useClerk()
+   const { user } = useUser()
+
+   async function becomeEducator() {
+      try {
+
+
+      } catch(err) {
+
+      }
+   }
+
 
    return (
       <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCoursesListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
          <img src={assets.logo} alt="Logo" className="w-28 lg:w-32 cursor-pointer"/>
          <div className="md:flex hidden items-center gap-5 text-gray-500">
             <div className="flex items-center gap-5">
-               <button>Become Educator</button>
-               | <Link to='/enrollments' >Enrollments</Link>
-               <button className="bg-blue-600 text-white px-5 py-2 rounded-full">Create Account</button>
+               {
+                  user && <Fragment>
+                     <button className="add-cursor" onClick={becomeEducator}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+                     | <Link to='/enrollments' >My Enrollments</Link>
+                  </Fragment>
+               }
+               {user
+                  ? <UserButton />
+                  : <button onClick={() => openSignIn()} className="bg-blue-600 text-white px-5 py-2 rounded-full">
+                     Create Account
+                  </button>}
             </div>
          </div>
          {/* For Phone Screens */}
          <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
             <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
-               <button>Become Educator</button>
-               | <Link to='/enrollments' >Enrollments</Link>
+               <button onClick={becomeEducator} className="add-cursor">{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+               | {
+               user && <Link to='/enrollments' >My Enrollments</Link>
+            }
             </div>
-            <button>
-               <img src={assets.user_icon} alt="User Icon" />
-            </button>
+            {user
+               ? <UserButton />
+               : <button onClick={() => openSignIn()}>
+                  <img src={assets.user_icon} alt="User icon" />
+               </button>}
          </div>
       </div>
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,23 @@
-
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './app/App.jsx'
 import {BrowserRouter} from 'react-router-dom';
 import {AppContextProvider} from './context/AppContext.jsx';
+import {ClerkProvider} from '@clerk/clerk-react';
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+   throw new Error('Missing Clerk Publishable Key in the .env file')
+}
 
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
-      <AppContextProvider>
-         <App/>
-      </AppContextProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+         <AppContextProvider>
+            <App/>
+         </AppContextProvider>
+      </ClerkProvider>
    </BrowserRouter>
 )
