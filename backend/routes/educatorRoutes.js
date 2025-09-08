8e9c966f3e815dcd2c034681a21de3dae03ca32c
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-09-08 07:44:52 -0500

    Add backend/middlewares/authMiddleware.js, addNewCourse function to backend/routes/educatorRoutes.js

--- backend/routes/educatorRoutes.js
+++ backend/routes/educatorRoutes.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import {updateRoleToEducator} from '../controllers/educatorController.js';
+import {
+   addNewCourse,
+   updateRoleToEducator
+} from '../controllers/educatorController.js';
+import upload from '../configs/multerConfig.js';
+import {authenticateEducator} from '../middlewares/authMiddleware.js';
 
 
 const router = express.Router();
 
 /************************* routes *************************/
 router.get('/update-role', updateRoleToEducator);
+router.post('/add-course',upload.single('image'), authenticateEducator, addNewCourse);
 
 
 
