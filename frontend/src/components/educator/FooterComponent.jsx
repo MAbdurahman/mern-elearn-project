937f58a602b4616b06c82535a5c6fd5561ac3ceb
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-09-18 05:04:37 -0500

    Improve frontend/src/components/educator/FooterComponent.jsx

--- frontend/src/components/educator/FooterComponent.jsx
+++ frontend/src/components/educator/FooterComponent.jsx
@@ -6,27 +6,22 @@ export default function FooterComponent() {
    let date = new Date();
    let year = date.getFullYear();
 
-
    return (
-      <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
-         <div className='flex items-center gap-4'>
-            <img className='hidden md:block w-20 footer-logo--educator' src={assets.mern_elearn_logo} alt="logo" />
-            <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
-            <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-               Copyright {year} eLearn,Inc., | All Rights Reserved.
-            </p>
-         </div>
-         <div className='flex items-center gap-3 max-md:mt-4'>
-            <a href="#">
-               <img src={assets.facebook_icon} alt="facebook_icon" />
+      <footer className='mt-2 flex flex-col  items-center justify-between text-left w-full px-8 border-t bg-gray-200'>
+         <div className='flex items-center gap-8 mt-3'>
+            <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'>
+               <img src={assets.facebook_icon} alt='facebook_icon' />
             </a>
-            <a href="#">
-               <img src={assets.twitter_icon} alt="twitter_icon" />
+            <a href='https://www.x.com/' target='_blank' rel='noopener noreferrer'>
+               <img src={assets.twitter_icon} alt='twitter_icon' />
             </a>
-            <a href="#">
-               <img src={assets.instagram_icon} alt="instagram_icon" />
+            <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'>
+               <img src={assets.instagram_icon} alt='instagram_icon' />
             </a>
          </div>
+         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
+            Copyright {year} eLearn,Inc., | All Rights Reserved.
+         </p>
       </footer>
 
    );
